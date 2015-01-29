e004fd70cd083098ba6d14b14ffb66683ef920f6
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2015-01-29 00:56:48 -0500

    Create categorical summary in PDF form.

--- expenses.py
+++ expenses.py
@@ -46,7 +46,7 @@ parser.add_argument('input', help='GnuCash database file or SQL path.')
 parser.add_argument('--accounts', nargs='+',
                     default=['Assets', 'Bank', 'Liabilities'],
                     help='Accounts to use for finding transactions.')
-# parser.add_argument('output', help='Output PDF file name.')
+parser.add_argument('--output', help='Output PDF file name.')
 args = parser.parse_args()
 
 # GnuCash uses '.' in the full name lookup for some reason
@@ -140,7 +140,116 @@ for k, v in sorted(expenses.items()):
     expense_local += local_sum
     expense_foreign += foreign_sum
 
+print()
 print('\tGrand Total\t\t%s%s\t%s%s' % (FOREIGN,
                                        expense_foreign.quantize(TWOPLACES),
                                        LOCAL,
                                        expense_local.quantize(TWOPLACES)))
+print()
+
+if not args.output:
+    exit()
+
+from reportlab.platypus import Paragraph, SimpleDocTemplate, Spacer, Table, TableStyle
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import inch, letter
+from reportlab.lib.styles import getSampleStyleSheet
+
+print('Writing report to %s ...' % (args.output, ))
+
+styles = getSampleStyleSheet()
+
+doc = SimpleDocTemplate(args.output, pagesize=letter)
+elements = []
+
+header = Paragraph('Transactions', styles['Heading1'])
+elements.append(header)
+
+data = sorted(set(data))  # FIXME: Splits are not unique, unfortunately.
+for date, num, split in data:
+    trans = split.parent
+    other = split.GetOtherSplit()
+
+    val = split.GetValue()
+    price = split.GetSharePrice()
+    curr = split.account.get_currency_or_parent()
+
+    #print('%s - %s' % (date, trans.GetDescription()))
+
+    if other:
+        other_name = other.account.get_full_name()
+        other_curr = other.account.get_currency_or_parent()
+        foreign = other.GetValue()
+        local = val.div(price, 1000, 0).neg()
+    #    print('\t%s %s%s' % (
+    #        other_name,
+    #        gnc_commodity_get_nice_symbol(other_curr),
+    #        foreign))
+    #    print('\t%s %s%s' % (
+    #        split.account.get_full_name(),
+    #        gnc_commodity_get_nice_symbol(curr),
+    #        local))
+    else:
+        pass
+    #    print('\t-')
+    #    print('\t%s %s%s' % (
+    #        split.account.get_full_name(),
+    #        gnc_commodity_get_nice_symbol(curr),
+    #        val.div(price, 1000, 0).neg()))
+
+space = Spacer(1, 2.5)
+elements.append(space)
+
+header = Paragraph('Expenses', styles['Heading1'])
+elements.append(header)
+
+expense_local = Decimal()
+expense_foreign = Decimal()
+for k, v in sorted(expenses.items()):
+    header = Paragraph(k, styles['Heading2'])
+    elements.append(header)
+
+    local_group = defaultdict(Decimal)
+    foreign_group = defaultdict(Decimal)
+    for name, local, foreign in v:
+        local = Decimal(local.num()) / Decimal(local.denom())
+        local_group[name] += local.quantize(TWOPLACES)
+        foreign = Decimal(foreign.num()) / Decimal(foreign.denom())
+        foreign_group[name] += foreign.quantize(TWOPLACES)
+
+    rows = []
+    local_sum = Decimal()
+    foreign_sum = Decimal()
+    for name in sorted(local_group):
+        local_val = local_group[name]
+        foreign_val = foreign_group[name]
+
+        local_sum += local_val
+        if local_val != foreign_val:
+            foreign_sum += foreign_val
+        else:
+            foreign_val = None
+
+        rows.append([name,
+                     (FOREIGN + str(foreign_val)) if foreign_val else '',
+                     LOCAL + str(local_val)])
+
+    rows.append(['Total',
+                 FOREIGN + str(foreign_sum),
+                 LOCAL + str(local_sum)])
+
+    t = Table(rows, [4.25 * inch, 1.25 * inch, 1 * inch])
+    t.setStyle(TableStyle([('ALIGNMENT', (1, 0), (-1, -1), 'RIGHT'),
+                           ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+                           ('FONT', (0, -1), (-1, -1), 'Helvetica-BoldOblique')]))
+    elements.append(t)
+
+    expense_local += local_sum
+    expense_foreign += foreign_sum
+
+#print('\tGrand Total\t\t%s%s\t%s%s' % (FOREIGN,
+#                                       expense_foreign.quantize(TWOPLACES),
+#                                       LOCAL,
+#                                       expense_local.quantize(TWOPLACES)))
+
+doc.build(elements)
